7244edad35a78490204e9672c74bfd8fce1e8673
Hevger Ibrahim <1067318@ucn.dk>
2019-11-06 14:19:26 +0100

    Create login layout

---
+++ web-client/src/components/header/Header.js
@@ -0,0 +1,99 @@
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import ProfileImage from "../../assets/images/profile.png";
+import Logo from "../../assets/images/senti.flow.negativ.png";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  headerCss: {
+    backgroundColor: "#1a1a32"
+  },
+  logoCss: {
+    height: "50px"
+  },
+  profileImageCss: {
+    height: "35px",
+    borderRadius: "50%"
+  }
+}));
+
+export default function Header() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = event => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <AppBar className={classes.headerCss} position="static">
+        <Toolbar>
+          <img className={classes.logoCss} src={Logo} alt="Senti Flow" />
+          <Typography variant="h6" className={classes.title}>
+            Aalborg Karneval Parade
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <img
+                  className={classes.profileImageCss}
+                  src={ProfileImage}
+                  alt="User image"
+                />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ web-client/src/components/login/Login.js
@@ -0,0 +1,128 @@
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import React from "react";
+import Logo from "../../assets/images/senti.flow.png";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Card from "@material-ui/core/Card";
+import IconButton from "@material-ui/core/IconButton";
+import AccountIcon from "@material-ui/icons/Person";
+import PasswordIcon from "@material-ui/icons/Visibility";
+import OrgIcon from "@material-ui/icons/Domain";
+
+const useStyles = makeStyles(theme => ({
+  gridFlex: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "50px"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    minWidth: "20%"
+  },
+  loginButton: {
+    marginTop: "20px",
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: "120px",
+    height: "36px",
+    background: "#ff9800",
+    color: "#FFFFFF",
+    fontWeight: "600",
+    "&:hover": {
+      background: "#e38d10"
+    },
+    "&:focus": {
+      border: "1px solid red"
+    }
+  },
+  logoCss: {
+    height: "70px"
+  },
+  card: {
+    boxSizing: "border-box"
+  }
+}));
+
+export default function Login() {
+  const classes = useStyles();
+
+  return (
+    <Grid className={classes.gridFlex} xs={12}>
+      <Card className={(classes.card, classes.gridFlex)}>
+        <img className={classes.logoCss} src={Logo} alt="Senti Flow" />
+
+        <TextField
+          id="outlined-email-input"
+          label="E-mail"
+          className={classes.textField}
+          type="email"
+          autoComplete="current-email"
+          margin="normal"
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton>
+                  <AccountIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          className={classes.textField}
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton>
+                  <PasswordIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+
+        <TextField
+          id="outlined-orgId-input"
+          label="Organisation ID"
+          className={classes.textField}
+          type="orgId"
+          autoComplete="current-orgId"
+          margin="normal"
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton>
+                  <OrgIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+
+        <Button
+          variant={"contained"}
+          fullWidth
+          color={"secondary"}
+          color="primary"
+          className={(classes.button, classes.loginButton)}
+        >
+          Login
+        </Button>
+      </Card>
+    </Grid>
+  );
+}
