d9ca2db6b74dc89f1578da0b69079830b9588c4d
Hevger Ibrahim <1067318@ucn.dk>
2019-11-11 13:58:29 +0100

    Setup Redux and login action

--- web-client/src/components/dashboard/Dashboard.js
+++ web-client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import MapView from "./MapView";
 import Filter from "./Filter";
 import StatusContainer from "./StatusContainer";
+import Header from "../header/Header";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -15,10 +16,13 @@ export default function Dashboard() {
   const classes = useStyles();
 
   return (
-    <Grid className={classes.container} container>
-      <MapView />
-      <Filter />
-      <StatusContainer />
-    </Grid>
+    <>
+      <Header />
+      <Grid className={classes.container} container>
+        <MapView />
+        <Filter />
+        <StatusContainer />
+      </Grid>
+    </>
   );
 }
--- web-client/src/components/login/Login.js
+++ web-client/src/components/login/Login.js
@@ -2,7 +2,9 @@ import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { loginUser } from "../../redux/action/authActions";
 import Logo from "../../assets/images/senti.flow.png";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Card from "@material-ui/core/Card";
@@ -45,80 +47,114 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function Login() {
+function Login(props) {
   const classes = useStyles();
+  const [state, setState] = useState({ email: "", password: "", orgId: "" });
+
+  function handleChange(e) {
+    setState({ ...state, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const userData = {
+      username: state.email,
+      password: state.password,
+      orgNickname: state.orgId
+    };
+    console.log(userData);
+    props.loginUser(userData);
+  }
 
   return (
-    <Grid className={classes.gridFlex} xs={12}>
-      <Card className={(classes.card, classes.gridFlex)}>
-        <img className={classes.logoStyle} src={Logo} alt="Senti Flow" />
+    <Grid item className={classes.gridFlex} xs={12}>
+      <form onSubmit={handleSubmit}>
+        <Card className={(classes.card, classes.gridFlex)}>
+          <img className={classes.logoStyle} src={Logo} alt="Senti Flow" />
+          <TextField
+            onChange={handleChange}
+            id="outlined-email-input"
+            name="email"
+            label="E-mail"
+            className={classes.textField}
+            type="email"
+            autoComplete="current-email"
+            margin="normal"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <AccountIcon />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
 
-        <TextField
-          id="outlined-email-input"
-          label="E-mail"
-          className={classes.textField}
-          type="email"
-          autoComplete="current-email"
-          margin="normal"
-          variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment>
-                <IconButton>
-                  <AccountIcon />
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-        />
+          <TextField
+            onChange={handleChange}
+            id="outlined-password-input"
+            name="password"
+            label="Password"
+            className={classes.textField}
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <PasswordIcon />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
 
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          className={classes.textField}
-          type="password"
-          autoComplete="current-password"
-          margin="normal"
-          variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment>
-                <IconButton>
-                  <PasswordIcon />
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-        />
+          <TextField
+            onChange={handleChange}
+            id="outlined-orgId-input"
+            name="orgId"
+            label="Organisation ID"
+            className={classes.textField}
+            type="orgId"
+            autoComplete="current-orgId"
+            margin="normal"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <OrgIcon />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+          />
 
-        <TextField
-          id="outlined-orgId-input"
-          label="Organisation ID"
-          className={classes.textField}
-          type="orgId"
-          autoComplete="current-orgId"
-          margin="normal"
-          variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment>
-                <IconButton>
-                  <OrgIcon />
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-        />
-
-        <Button
-          variant={"contained"}
-          fullWidth
-          color={"secondary"}
-          className={(classes.button, classes.loginButton)}
-        >
-          Login
-        </Button>
-      </Card>
+          <Button
+            type="submit"
+            variant={"contained"}
+            fullWidth
+            color={"secondary"}
+            className={(classes.button, classes.loginButton)}
+          >
+            Login
+          </Button>
+        </Card>
+      </form>
     </Grid>
   );
 }
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
