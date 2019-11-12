5645b16ef6bef8aa1312081ec409832277a61fbd
Hevger Ibrahim <1067318@ucn.dk>
2019-11-12 11:46:05 +0100

    Login partial implementation

--- web-client/src/components/dashboard/Dashboard.js
+++ web-client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory, useLocation } from "react-router";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import MapView from "./MapView";
 import Filter from "./Filter";
 import StatusContainer from "./StatusContainer";
 import Header from "../header/Header";
+import { setToken } from "../../redux/action/authActions";
+import cookie from "react-cookies";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -14,6 +17,15 @@ const useStyles = makeStyles(theme => ({
 
 export default function Dashboard() {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    var loginData = cookie.load("SESSION");
+    if (!loginData) {
+      history.push("/login");
+    }
+  }, [location, history]);
 
   return (
     <>
