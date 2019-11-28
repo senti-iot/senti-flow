e0f6cc30e368ea555c859aa693a605909923ba75
Darwish Mammo <darwish.mammo@gmail.com>
2019-11-28 14:54:58 +0100

    Remove square around profile image effect

--- mobile-client/components/Header.js
+++ mobile-client/components/Header.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Image,
   StyleSheet,
   TouchableHighlight,
   AsyncStorage
-} from "react-native";
-import { Appbar, Avatar, Menu } from "react-native-paper";
-import Logo from "../assets/logo1.png";
-import Profile from "../assets/profile.png";
+} from 'react-native';
+import { Appbar, Avatar, Menu } from 'react-native-paper';
+import Logo from '../assets/logo1.png';
+import Profile from '../assets/profile.png';
 
 const Header = props => {
   const [menuVisible, setMenuVisible] = useState(false);
   const [userAvatar, setUserAvatar] = useState(
-    "https://www.gravatar.com/avatar/0?s=200"
+    'https://www.gravatar.com/avatar/0?s=200'
   );
   handleMenuVisibale = () => setMenuVisible(!menuVisible);
 
   const getUserAvatar = async () => {
-    await AsyncStorage.getItem("userAvatar").then(avatar => {
+    await AsyncStorage.getItem('userAvatar').then(avatar => {
       setUserAvatar(avatar);
     });
   };
@@ -28,19 +28,22 @@ const Header = props => {
 
   return (
     <Appbar.Header style={styles.header}>
-      <Image resizeMode={"contain"} style={styles.logo} source={Logo} />
+      <Image resizeMode={'contain'} style={styles.logo} source={Logo} />
       {props.authorized ? (
         <Menu
           style={styles.menu}
           onDismiss={handleMenuVisibale}
           anchor={
-            <TouchableHighlight onPress={handleMenuVisibale}>
+            <TouchableHighlight
+              underlayColor={'transparent'}
+              onPress={handleMenuVisibale}
+            >
               <Avatar.Image size={45} source={{ uri: userAvatar }} />
             </TouchableHighlight>
           }
           visible={menuVisible}
         >
-          <Menu.Item onPress={() => props.logOut()} title="Logout" />
+          <Menu.Item onPress={() => props.logOut()} title='Logout' />
         </Menu>
       ) : null}
     </Appbar.Header>
@@ -49,14 +52,14 @@ const Header = props => {
 
 const styles = StyleSheet.create({
   header: {
-    paddingLeft: "5%",
-    paddingRight: "5%",
-    paddingBottom: "5%",
+    paddingLeft: '5%',
+    paddingRight: '5%',
+    paddingBottom: '5%',
     height: 75,
-    backgroundColor: "#1a1a32",
-    alignItems: "center",
-    alignContent: "center",
-    justifyContent: "space-between"
+    backgroundColor: '#1a1a32',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between'
   },
   menu: { marginTop: 50 },
   logo: {
