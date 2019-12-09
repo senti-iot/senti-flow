07aa0ca6dc224bd1ef3eff0cbd924b569b5485b8
Hevger Ibrahim <1067318@ucn.dk>
2019-12-09 10:12:41 +0100

    Fixed bug with status buttons

--- mobile-client/components/Home.js
+++ mobile-client/components/Home.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-import { View, AsyncStorage, StyleSheet } from 'react-native';
-import * as mqttActions from '../utilities/MQTT';
-import StatusButton from './StatusButton';
+import React, { useState } from "react";
+import { View, AsyncStorage, StyleSheet } from "react-native";
+import * as mqttActions from "../utilities/MQTT";
+import StatusButton from "./StatusButton";
 
 mqttActions.makeConnection();
 
 const sendStatus = async status => {
-  data = { userStatus: status, userID: await AsyncStorage.getItem('userID') };
-  mqttActions.sendData(JSON.stringify(data), 'userStatus');
+  data = { userStatus: status, userID: await AsyncStorage.getItem("userID") };
+  mqttActions.sendData(JSON.stringify(data), "userStatus");
 };
 const Home = () => {
   const [status, setStatus] = useState([
-    { title: 'Alt Ok', isActive: true },
-    { title: 'Stå Stille', isActive: false },
-    { title: 'Træghed', isActive: false },
-    { title: 'Brug for samarit', isActive: false },
-    { title: 'Politi!', isActive: false }
+    { title: "Alt Ok", isActive: false },
+    { title: "Stå Stille", isActive: false },
+    { title: "Træghed", isActive: false },
+    { title: "Brug for samarit", isActive: false },
+    { title: "Politi!", isActive: false }
   ]);
 
   const handleActive = i => {
     const updatedStatus = [...status];
     updatedStatus.forEach((s, idx) => {
-      idx === i ? (s.isActive = !s.isActive) : (s.isActive = false);
+      idx === i ? (s.isActive = true) : (s.isActive = false);
     });
     setStatus(updatedStatus);
   };
@@ -29,53 +29,53 @@ const Home = () => {
   return (
     <View style={styles.home}>
       <StatusButton
-        color='#6dd400'
-        label='White'
+        color="#6dd400"
+        label="White"
         active={status[0].isActive}
         onPress={() => {
-          sendStatus('altOk');
+          sendStatus("altOk");
           handleActive(0);
         }}
         title={status[0].title}
       />
 
       <StatusButton
-        color='#ffd200'
+        color="#ffd200"
         active={status[1].isActive}
         onPress={() => {
-          sendStatus('staaStille');
+          sendStatus("staaStille");
           handleActive(1);
         }}
         title={status[1].title}
       />
 
       <StatusButton
-        color='#ffd200'
+        color="#ffd200"
         active={status[2].isActive}
         onPress={() => {
-          sendStatus('traeghed');
+          sendStatus("traeghed");
           handleActive(2);
         }}
         title={status[2].title}
       />
 
       <StatusButton
-        color='#e01f20'
-        label='White'
+        color="#e01f20"
+        label="White"
         active={status[3].isActive}
         onPress={() => {
-          sendStatus('samarit');
+          sendStatus("samarit");
           handleActive(3);
         }}
         title={status[3].title}
       />
 
       <StatusButton
-        color='#e01f20'
-        label='White'
+        color="#e01f20"
+        label="White"
         active={status[4].isActive}
         onPress={() => {
-          sendStatus('politi');
+          sendStatus("politi");
           handleActive(4);
         }}
         title={status[4].title}
@@ -88,8 +88,8 @@ const styles = StyleSheet.create({
   home: {
     flex: 1,
     padding: 10,
-    flexDirection: 'column',
-    justifyContent: 'space-between'
+    flexDirection: "column",
+    justifyContent: "space-between"
   }
 });
 
