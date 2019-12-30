e50c3a8cd3d4e30412732c8dc238f2849311ae98
Hevger Ibrahim <1067318@ucn.dk>
2019-12-30 01:11:35 +0100

    Add marker icon

--- web-client/src/components/dashboard/MapView.js
+++ web-client/src/components/dashboard/MapView.js
@@ -1,18 +1,30 @@
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import { Map, Marker, TileLayer } from "react-leaflet";
+import { Map, Marker, TileLayer, Popup } from "react-leaflet";
 import MessageBox from "./MessageBox";
+import L from "leaflet";
 
 const useStyles = makeStyles(theme => ({
   mapStyle: {
     height: "80vh",
     zIndex: "1"
+  },
+  divIcon: {
+    border: "5px solid green",
+    borderRadius: 50
   }
 }));
 
 export default function MapView({ guards }) {
   const classes = useStyles();
+
+  var myIcon = L.icon({
+    className: classes.divIcon,
+    iconUrl: "https://starbyface.com/ImgBase/testPhoto/test1.jpg",
+    iconRetinaUrl: "https://starbyface.com/ImgBase/testPhoto/test1.jpg",
+    iconSize: [30, 30]
+  });
 
   return (
     <Grid className={classes.mapStyle} xs={7} item={true}>
@@ -33,7 +45,12 @@ export default function MapView({ guards }) {
               guard.guardLocation.latitude,
               guard.guardLocation.longitude
             ]}
-          />
+            icon={myIcon}
+          >
+            <Popup>
+              <span>Guard Name</span>
+            </Popup>
+          </Marker>
         ))}
       </Map>
       <MessageBox />
